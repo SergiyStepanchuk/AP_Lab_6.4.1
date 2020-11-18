10ff5acd9bcdf43472d56f7a9efe5a5e59546e0d
SergiyStepanchuk <prochliste@gmail.com>
2020-11-18 21:34:12 +0200

    dorob

--- Program/main.cpp
+++ Program/main.cpp
@@ -37,6 +37,21 @@ double sum_dod(double* mass, int size) {
 	return sum;
 }
 
+int min_(double* mass, int size) {
+	int min_i = 0;
+	for (--size; size > 0; --size)
+		if (mass[min_i] > mass[size])
+			min_i = size;
+	return min_i;
+}
+
+double sum(double* mass, int size) {
+	double sum = 0;
+	for (--size; size >= 0; size--)
+		sum += mass[size];
+	return sum;
+}
+
 void sort_half(double* mass, const int &size) {
 	for (int i = 0; i < size - 1; i+=2) { // бульбашка
 		for (int j = 0; j < size - i - 1; j+=2) {
@@ -65,11 +80,16 @@ void main() {
 	double* mass = new double[n];
 	full_mass<double>(mass, n, dia);
 	draw_mass(mass, n, c_space);
-	sort_half(mass, n);
-	draw_mass(mass, n, c_space);
+	
 	cout.precision(10);
 	
-	cout << "sum dod: " << sum_dod(mass, n) << endl;
+	cout << "1.1 sum dod: " << sum_dod(mass, n) << endl;
+	cout << "1.2 min index: " << min_(mass, n) << endl;
+	cout << "1.2 sum to min: " << sum(mass, min_(mass, n)) << endl;
+	cout << "2. sort_half" << endl;
+	cout.precision(2);
+	sort_half(mass, n);
+	draw_mass(mass, n, c_space);
 
 	delete[] mass;
 }
